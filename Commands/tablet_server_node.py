f4b07e32006fe2a6c87378ea87a0254a84bf8644
kekraft <korykraft@hotmail.com>
2015-08-12 16:08:05 -0700

    Tablet server node that works.

--- Commands/tablet_server_node.py
+++ Commands/tablet_server_node.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float64MultiArray
+
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
 
@@ -10,7 +11,8 @@ from twisted.internet.protocol import Factory, Protocol
 
 class RobotComm(Protocol):
     def __init__(self):
-        self.publisher = rospy.Publisher('/tablet_command', String)  # ROS Publisher
+        self.publisher = rospy.Publisher('/tablet_command', Float64MultiArray)  # ROS Publisher
+        
 
 
     def connectionMade(self):
@@ -22,40 +24,42 @@ class RobotComm(Protocol):
         self.factory.clients.remove(self)
 
     def dataReceived(self, data):
-    	print "Data received"
-    	print data
+        print "Data received"
+        print data
        
         
         msg = data.split(',')
+        print msg
 
         # get behavior type
-    	if msg[0] == "CIRCLE":
-    		behavior_type = 0
-    	elif msg[0] == "LINE":
-    		behavior_type = 1
-    	elif msg[0] == "PYRAMID":
-    		behavior_type = 2
-    	elif msg[0] == "DIRECTION":
-    		behavior_type = 3
-    	elif msg[0] == "KILL":
-    		behavior_type = 4
-    	else:
-    		behavior_type = -1
+        if msg[0] == "CIRCLE":
+            behavior_type = 0
+        elif msg[0] == "LINE":
+            behavior_type = 1
+        elif msg[0] == "PYRAMID":
+            behavior_type = 2
+        elif msg[0] == "DIRECTION":
+            behavior_type = 3
+        elif msg[0] == "KILL":
+            behavior_type = 4
+        else:
+            behavior_type = -1
 
-    	# parse location	
-    	x = float(msg[1])
-    	y = float(msg[2])
-    	z = float(msg[3])
-    	theta = float(msg[4])
+        # parse location    
+        x = float(msg[1])
+        y = float(msg[2])
+        z = float(msg[3])
+        theta = float(msg[4])
 
-    	location = (x, y, z, theta)
+        location = (x, y, z, theta)
 
-    	print "Type: ", behavior_type
-    	print "Location: ", location
+        print "Type: ", behavior_type
+        print "Location: ", location
 
         # Publish via ROS
         msg = Float64MultiArray()
         msg.data = [behavior_type, x, y, z, theta]
+        print msg.data
         self.publisher.publish(msg)
 
         for c in self.factory.clients:
@@ -65,16 +69,16 @@ class RobotComm(Protocol):
 
     def message_position(self):
         # list of num robots, followed by csv x,y,theta     
-        bot = [x for x in xrange(1,10)]
-        num_bots = 3
-        msg = str(num_bots) + ","
+        # bot = [x for x in xrange(1,10)]
+        # num_bots = 3
+        # msg = str(num_bots) + ","
 
-        for bot in num_bots:
-            x = bot[0]
-            y = bot[1]
-            z = bot[2]
-            msg = msg + x + "," + y + "," + z + "," 
-
+        # for bot in num_bots:
+        #     x = bot[0]
+        #     y = bot[1]
+        #     z = bot[2]
+        #     msg = msg + x + "," + y + "," + z + "," 
+        msg = "hello there"
 
         self.transport.write(msg + '\n')
 
@@ -86,12 +90,14 @@ def main():
     # Run ROS node
     rospy.init_node('tablet_server')
 
-	# Start the server/reactor loop
-	factory = Factory()
-	factory.protocol = RobotComm
-	factory.clients = []
-	reactor.listenTCP(11411, factory)
-	reactor.run()
+    # Start the server/reactor loop
+    factory = Factory()
+    factory.protocol = RobotComm
+    factory.clients = []
+    reactor.listenTCP(11411, factory)
+    reactor.run()
+
+    rospy.spin()
 
 if __name__ == '__main__':
-	main()
+    main()
