5f8dbbfd2d88e0cfe17e2c48f8ae449c7fab1435
kekraft <korykraft@hotmail.com>
2015-08-12 14:56:06 -0700

    Updated server code to parse type and location

--- Commands/server.py
+++ Commands/server.py
@@ -3,6 +3,9 @@
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
 
+## behavior types circle, line, pyramid, direction (dummies for the others)
+## location that is comma separated, x, y, z, theta
+
 class RobotComm(Protocol):
     def connectionMade(self):
         print "a client connected"
@@ -15,23 +18,26 @@ class RobotComm(Protocol):
     def dataReceived(self, data):
     	print "Data received"
     	print data
-    # a = data.split(':')
-    # print a
-    # if len(a) > 1:
-    #     command = a[0]
-    #     content = a[1]
+       
+        # get behavior type
+        msg = data.split(',')
+    	if msg[0] == "CIRCLE":
+    		behavior_type = 0
+    	elif msg[0] == "LINE":
+    		behavior_type = 1
+    	elif msg[0] == "PYRAMID":
+    		behavior_type = 2
 
-    #     msg = ""
-    #     if command == "iam":
-    #         self.name = content
-    #         msg = self.name + " has joined"
+    	# parse location	
+    	x = float(msg[1])
+    	y = float(msg[2])
+    	z = float(msg[3])
+    	theta = float(msg[4])
 
-    #     elif command == "msg":
-    #         msg = self.name + ": " + content
-    #         print msg
+    	location = (x, y, z, theta)
 
-    #     for c in self.factory.clients:
-    #         c.message(msg)
+    	print "Type: ", behavior_type
+    	print "Location: ", location
 
     def message(self, message):
         self.transport.write(message + '\n')
@@ -43,7 +49,7 @@ def main():
 	factory = Factory()
 	factory.protocol = RobotComm
 	factory.clients = []
-	reactor.listenTCP(11311, factory)
+	reactor.listenTCP(11411, factory)
 	reactor.run()
 
 if __name__ == '__main__':
