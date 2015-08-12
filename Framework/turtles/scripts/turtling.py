0feef19be7f4bcd7ace68e3fb25788ceb64df60c
Cameron Bowie <bowiec@onid.oregonstate.edu>
2015-08-12 15:42:15 -0700

    mostly done

--- Framework/turtles/scripts/turtling.py
+++ Framework/turtles/scripts/turtling.py
@@ -3,27 +3,81 @@
 import socket
 import time
 import random
+import sys
+import rospy
+import actionlib
+from geometry_msgs.msg import Pose
+from std_msgs.msg import String
+
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory, Protocol
 
 
-class Turtling:
+class Turtling(Protocol):
 	def __init__(self, ip, port):
 		self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.clientsocket.connect((ip, port))
-		self.ip = "123.123.123.123"
+		self.ip = ip
 		self.id = random.randint(1, 100000)
+
+		self.actionclient = actionlib.SimpleActionClient('move_base', actionlib_tutorials.msg.FibonacciAction)
 
 	def pose_callback(self, msg):
 		pose = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
 
-		self.clientsocket.send(self.ip + str(pose) + str(self.id))
+		self.clientsocket.send("(" + self.ip + "," + str(pose) + ")")
+
+	def connectionMade(self):
+		print "a client connected"
+		self.factory.clients.append(self)
+		print "clients are ", self.factory.clients
+
+	def connectionLost(self, reason):
+		self.factory.clients.remove(self)
+
+	def dataReceived(self, data):
+		print "Data received"
+		print data
+
+		frame_id, pose = eval(data)
+
+client = actionlib.SimpleActionClient('fibonacci', actionlib_tutorials.msg.FibonacciAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = actionlib_tutorials.msg.FibonacciGoal(order=20)
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()
 
 
 
+if __name__ == '__main__':
+	ip = sys.argv[1]
+	port_listen = int(sys.argv[2])
+	port_talk = int(sys.argv[3])
+
+	rospy.init_node('turtling', anonymous=True)
+
+	turtling = Turtling(ip, port_talk)
+
+	# need subscriber for pose
+	rospy.Subscriber("/amcl_pose", String, lambda x: turtling.pose_callback(x))
 
 
-clientsocket.connect(('10.214.152.40', 8089))
-# clientsocket.connect(('10.214.152.185', 12397))
 
-clientsocket.send('("132.123.123.123",(1,2,3),533651)')
-
-clientsocket.close()
+	# Start the server/reactor loop
+	factory = Factory()
+	factory.protocol = turtling
+	factory.clients = []
+	reactor.listenTCP(port_listen, factory)
+	reactor.run()
