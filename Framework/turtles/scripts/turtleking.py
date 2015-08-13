5f925162f6ab6da3ec0276092decfde94d2560b3
Cameron Bowie <bowiec@onid.oregonstate.edu>
2015-08-12 17:01:54 -0700

    stuff done???

--- Framework/turtles/scripts/turtleking.py
+++ Framework/turtles/scripts/turtleking.py
@@ -1,54 +1,130 @@
 #!/usr/bin/env/python
 
+import rospy
+
+from move_base_msgs.msg import MoveBase
+from std_msgs.msg import Int
+from geometry_msgs.msg import Pose
+
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
 
-class RobotComm(Protocol):
-    def __init__(self):
-        client_number = 0
-        d = dict()
-    def connectionMade(self):
-        print "a client connected"
-        self.factory.clients.append(self)
-        print "clients are ", self.factory.clients
-        client_number = client_number+1
+class Turtleking(Protocol):
+	def __init__(self, ip, port):
+		self.ip = ip
 
-    def connectionLost(self, reason):
-        self.factory.clients.remove(self)
-        client_number = client_number-1
+		self.client_number = 0
+		self.d = dict()
+		self.pending = {}
+		self.active = {}
+		self.clients = {}
 
+		self.turtles_pub = rospy.Publisher("/turtles", Int)
 
-    def dataReceived(self, data):
-    	print "Data received"
-    	print data
-    	ip, pos = eval(data)
-        print ip
-        print pos
-        key = ip
-    for i in xrange(100):
-        if key in d:
-            frame_id = d[key][1]
-            data = [pos,frame_id]
-            dict[ip] = (data)
-        else:
-            frame_id = "turtlebot_%d" % client_number
-            data = [pos,frame_id]
-            dict[ip] = (data)
-        
-        
+	def connectionMade(self):
+		print "a client connected"
+		self.factory.clients.append(self)
+		print "clients are ", self.factory.clients
+		self.client_number = self.client_number+1
 
-    def message(self, message):
-        self.transport.write(message + '\n')
+		self.clients[self] = self.client_number
 
+		self.turtles_pub.publish(self.client_number)
 
-def main():
-	# Start the server/reactor loop
+	def connectionLost(self, reason):
+		self.factory.clients.remove(self)
+		self.turtles_pub.publish(-self.clients[self])
 
-	factory = Factory()
-	factory.protocol = RobotComm
-	factory.clients = []
-	reactor.listenTCP(8089, factory)
-	reactor.run()
+	def dataReceived(self, data):
+		print "Data received"
+		print data
+		client_ip, pos, feedback = eval(data)
+		print client_ip
+		print pos
+		print feedback
+		key = client_ip
+
+		if key in self.d.keys():
+			if feedback == "success":
+				for val in self.active.values():
+					if val != None:
+						done = False
+					else:
+						done = True
+
+				if done:
+					for (key, value) in self.d.items():
+						if len(self.pending[key]) > 0:
+							value[3].send("('" + self.pending[key][0][1] + "'," + self.pending[key][0][1] + ")")
+							self.active[key] = self.pending[key][0]
+							self.pending[key] = self.pending[key][1:]
+						else:
+							self.active[key] = None
+
+			elif feedback == "failure":
+				print "oh shit!"
+
+			elif feedback == None:
+				self.d[key][0] = pos
+				self.publish_pose(self.d[key])
+
+			else:
+				print "also, oh shit!"
+
+		else:
+			frame_id = "turtlebot_%d" % client_number
+
+			sub = rospy.Subscriber(frame_id + "/nav_goal", MoveBase, lambda x: self.add_goal(x, client_ip))
+			pub_pose = rospy.Publisher(frame_id + "/amcl_pose", Pose)
+			# pub_feedback = rospy.Publisher()
+			connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			connection.connect((ip, port))
+
+			data = [pos,frame_id,sub,connection, pub_pose]
+			self.d[client_ip] = data
+
+	def add_goal(self, msg, client_id):
+		frame_id = msg.target_pose.header.frame_id
+
+		pose = (msg.target_pose.pose.position.x,
+				msg.target_pose.pose.position.y,
+				msg.target_pose.pose.position.z,
+				msg.target_pose.pose.orientation.x,
+				msg.target_pose.pose.orientation.y,
+				msg.target_pose.pose.orientation.z,
+				msg.target_pose.pose.orientation.w)
+
+		self.pending[client_id].append((seq, frame_id, pose))
+		# double check this!!!!!
+		self.pending[client_id].sort(key=lambda x: x[0])
+
+		
+
+	def publish_pose(self, data):
+		pose = Pose()
+
+		pose.position.x = data[0][0]
+		pose.position.y = data[0][1]
+		pose.position.z = data[0][2]
+		pose.orientation.x = data[0][3]
+		pose.orientation.y = data[0][4]
+		pose.orientation.z = data[0][5]
+		pose.orientation.w = data[0][6]
+
+		data[4].publish(pose)
+
 
 if __name__ == '__main__':
-	main()
+	ip = sys.argv[1]
+	port = int(sys.argv[2])
+
+	rospy.init_node('turtleking', anonymous=True)
+
+	turtleking = Turtleking(ip, port_talk)
+
+	# Start the server/reactor loop
+	factory = Factory()
+	factory.protocol = turtleking
+	factory.clients = []
+	reactor.listenTCP(port_listen, factory)
+	reactor.run()
--- Framework/turtles/scripts/turtling.py
+++ Framework/turtles/scripts/turtling.py
@@ -8,6 +8,7 @@ import rospy
 import actionlib
 from geometry_msgs.msg import Pose
 from std_msgs.msg import String
+from move_base_msgs.msg import MoveBase
 
 from twisted.internet import reactor
 from twisted.internet.protocol import Factory, Protocol
@@ -18,14 +19,16 @@ class Turtling(Protocol):
 		self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.clientsocket.connect((ip, port))
 		self.ip = ip
-		self.id = random.randint(1, 100000)
 
-		self.actionclient = actionlib.SimpleActionClient('move_base', actionlib_tutorials.msg.FibonacciAction)
+		self.actionclient = actionlib.SimpleActionClient('move_base', MoveBase)
+		self.actionclient.wait_for_server()
 
 	def pose_callback(self, msg):
-		pose = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)
+		pose = (msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
+				msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z,
+				msg.pose.orientation.w)
 
-		self.clientsocket.send("(" + self.ip + "," + str(pose) + ")")
+		self.clientsocket.send("('" + self.ip + "'," + str(pose) + "," + feedback + ")")
 
 	def connectionMade(self):
 		print "a client connected"
@@ -41,23 +44,26 @@ class Turtling(Protocol):
 
 		frame_id, pose = eval(data)
 
-client = actionlib.SimpleActionClient('fibonacci', actionlib_tutorials.msg.FibonacciAction)
+		goal.target_pose.header.frame_id = frame_id
+		goal.target_pose.header.stamp = rospy.Time.now()
 
-    # Waits until the action server has started up and started
-    # listening for goals.
-    client.wait_for_server()
+		goal.target_pose.pose.position.x = pose[0]
+		goal.target_pose.pose.position.y = pose[1]
+		goal.target_pose.pose.position.z = pose[2]
 
-    # Creates a goal to send to the action server.
-    goal = actionlib_tutorials.msg.FibonacciGoal(order=20)
+		goal.target_pose.pose.orientation.x = pose[3]
+		goal.target_pose.pose.orientation.y = pose[4]
+		goal.target_pose.pose.orientation.z = pose[5]
+		goal.target_pose.pose.orientation.w = pose[6]
 
-    # Sends the goal to the action server.
-    client.send_goal(goal)
+		self.actionclient.sendGoal(goal)
 
-    # Waits for the server to finish performing the action.
-    client.wait_for_result()
+		client.wait_for_result()
 
-    # Prints out the result of executing the action
-    return client.get_result()
+		# feedback = client.get_result()
+		feedback = "success"
+
+		self.clientsocket.send("('" + self.ip + "', (None, None, None)," + feedback + ")")
 
 
 
@@ -72,8 +78,6 @@ if __name__ == '__main__':
 
 	# need subscriber for pose
 	rospy.Subscriber("/amcl_pose", String, lambda x: turtling.pose_callback(x))
-
-
 
 	# Start the server/reactor loop
 	factory = Factory()
